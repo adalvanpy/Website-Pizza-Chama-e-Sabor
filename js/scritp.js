8fd570a340dbe3f542190a7dcde77351b8479cde
adalvanpy <dalvan24.lima@gmail.com>
2024-11-15 18:45:56 -0300

    primeiro commit

---
+++ js/scritp.js
@@ -0,0 +1,53 @@
+// menu
+
+//var menuu = document.getElementById("menuu");
+//console.log(menuu);
+
+//var abrirMenu = document.getElementById("abrirMenu");
+
+//var fecharMenu = document.getElementById("fecharMenu");
+
+//abrirMenu.onclick = function() {
+//menuu.style.display = "block";
+//};
+
+//fecharMenu.onclick = function() {
+//menuu.style.display = "none";
+//};
+//window.onclick = function(event) {
+//if (event.target == menuu) {
+//   menuu.style.display = "none";
+//}
+//};
+
+ // pizza calabresa
+ //var cal = document.getElementById("call");
+ //var opencal = document.getElementById("abrirCal");
+ //var closecal = document.getElementById("fecharCal");
+
+ //opencal.onclick = function() {
+ //cal.style.display = 'block';
+ //};
+
+ //closecal.onclick = function() {
+ //cal.style.display = 'none';
+ //};
+
+ //cal.onclick = function(event) {
+  //  if (event.target === cal) {
+  //     cal.style.display = 'none';
+  //  }
+ // };
+//endereco
+
+//var endereco = document.getElementById("endr");
+//var abrirEnd = document.getElementById("abrirEnd");
+//var fecharEnd = document.getElementById("fecharEnd");
+
+//abrirEnd.onclick = function() {
+//   endereco.style.display = 'block';
+//}
+
+//fecharEnd.onclick = function() {
+ //  endereco.style.display = 'none';
+//}
